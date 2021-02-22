66575573dec53b2f2eaa820ab2ae1be1952e8e83
Suraj Kota <surakota@amazon.com>
2021-02-22 21:29:44 +0000

    chore: migrate repo to new format

--- pkg/resource/training_job/identifiers.go
+++ pkg/resource/training_job/identifiers.go
@@ -16,7 +16,7 @@
 package training_job
 
 import (
-	ackv1alpha1 "github.com/aws/aws-controllers-k8s/apis/core/v1alpha1"
+	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 )
 
 // resourceIdentifiers implements the
