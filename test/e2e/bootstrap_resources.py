22d288230c4c4e8cc75b44f9f10e40aa0551ea47
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2021-04-15 19:01:49 -0700

    Migrate community e2e tests (#13)
    * Migrate community e2e tests
    
    * Clean workflow and update acktest
    
    * Black formatted

---
+++ test/e2e/bootstrap_resources.py
@@ -0,0 +1,41 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# 	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+"""Declares the structure of the bootstrapped resources and provides a loader
+for them.
+"""
+
+from dataclasses import dataclass
+from acktest.resources import read_bootstrap_config
+from e2e import bootstrap_directory
+
+SAGEMAKER_SOURCE_DATA_BUCKET = "source-data-bucket-718865417152-us-west-2"
+
+
+@dataclass
+class TestBootstrapResources:
+    DataBucketName: str
+    ExecutionRoleARN: str
+
+
+_bootstrap_resources = None
+
+
+def get_bootstrap_resources(bootstrap_file_name: str = "bootstrap.yaml"):
+    global _bootstrap_resources
+    if _bootstrap_resources is None:
+        _bootstrap_resources = TestBootstrapResources(
+            **read_bootstrap_config(
+                bootstrap_directory, bootstrap_file_name=bootstrap_file_name
+            ),
+        )
+    return _bootstrap_resources
---
+++ test/e2e/service_bootstrap.py
@@ -0,0 +1,110 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# 	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+"""Bootstraps the resources required to run the SageMaker integration tests.
+"""
+
+import boto3
+import json
+import logging
+import time
+
+from acktest import resources
+from acktest.aws.identity import get_region, get_account_id
+from acktest.aws.s3 import duplicate_bucket_contents
+from e2e import bootstrap_directory
+from e2e.bootstrap_resources import TestBootstrapResources, SAGEMAKER_SOURCE_DATA_BUCKET
+
+
+def create_execution_role() -> str:
+    region = get_region()
+    role_name = resources.random_suffix_name(f"ack-sagemaker-execution-role", 63)
+    iam = boto3.client("iam", region_name=region)
+
+    iam.create_role(
+        RoleName=role_name,
+        AssumeRolePolicyDocument=json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {"Service": "sagemaker.amazonaws.com"},
+                        "Action": "sts:AssumeRole",
+                    }
+                ],
+            }
+        ),
+        Description="SageMaker execution role for ACK integration and canary tests",
+    )
+
+    iam.attach_role_policy(
+        RoleName=role_name,
+        PolicyArn="arn:aws:iam::aws:policy/AmazonSageMakerFullAccess",
+    )
+    iam.attach_role_policy(
+        RoleName=role_name, PolicyArn="arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    )
+
+    iam_resource = iam.get_role(RoleName=role_name)
+    resource_arn = iam_resource["Role"]["Arn"]
+
+    # There appears to be a delay in role availability after role creation
+    # resulting in failure that role is not present. So adding a delay
+    # to allow for the role to become available
+    time.sleep(10)
+    logging.info(f"Created SageMaker execution role {resource_arn}")
+
+    return resource_arn
+
+
+def create_data_bucket() -> str:
+    region = get_region()
+    account_id = get_account_id()
+    bucket_name = resources.random_suffix_name(
+        f"ack-data-bucket-{region}-{account_id}", 63
+    )
+
+    s3 = boto3.client("s3", region_name=region)
+    if region == "us-east-1":
+        s3.create_bucket(Bucket=bucket_name)
+    else:
+        s3.create_bucket(
+            Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": region}
+        )
+
+    logging.info(f"Created SageMaker data bucket {bucket_name}")
+
+    s3_resource = boto3.resource("s3", region_name=region)
+
+    source_bucket = s3_resource.Bucket(SAGEMAKER_SOURCE_DATA_BUCKET)
+    destination_bucket = s3_resource.Bucket(bucket_name)
+    duplicate_bucket_contents(source_bucket, destination_bucket)
+
+    logging.info(f"Synced data bucket")
+
+    return bucket_name
+
+
+def service_bootstrap() -> dict:
+    logging.getLogger().setLevel(logging.INFO)
+
+    return TestBootstrapResources(
+        create_data_bucket(),
+        create_execution_role(),
+    ).__dict__
+
+
+if __name__ == "__main__":
+    config = service_bootstrap()
+    # Write config to current directory by default
+    resources.write_bootstrap_config(config, bootstrap_directory)
