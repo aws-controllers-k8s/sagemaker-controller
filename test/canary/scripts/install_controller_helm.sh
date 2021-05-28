e0c27b8a8d49098681eb25a8a2785bd32a836b07
Suraj Kota <surakota@amazon.com>
2021-05-28 06:12:17 +0000

    add account_id to helm charts

--- test/canary/scripts/install_controller_helm.sh
+++ test/canary/scripts/install_controller_helm.sh
@@ -7,10 +7,12 @@ function install_helm_chart() {
     local oidc_role_arn="$2"
     local region="$3"
     local namespace="$4"
+    local account_id=$(aws sts get-caller-identity --output text --query "Account")
 
     yq w -i helm/values.yaml "serviceAccount.annotations" ""
     yq w -i helm/values.yaml 'serviceAccount.annotations."eks.amazonaws.com/role-arn"' "$oidc_role_arn"
     yq w -i helm/values.yaml "aws.region" $region
+    yq w -i helm/values.yaml "aws.account_id" $account_id
 
     kubectl create namespace $namespace
     kubectl apply -f helm/crds
