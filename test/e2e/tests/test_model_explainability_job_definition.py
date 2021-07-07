cc448c2bac57819befc1340d9b644307d035c892
Steakley <ryanstea@amazon.com>
2021-07-07 14:02:02 -0700

    Adds a logging debugger statement to see when resource_arn is None what the status was and add comment on why using a random sleep and add human readable message for logging.debug Additionally pins to latest test infra

--- test/e2e/tests/test_model_explainability_job_definition.py
+++ test/e2e/tests/test_model_explainability_job_definition.py
@@ -53,11 +53,11 @@ def xgboost_churn_model_explainability_job_definition(xgboost_churn_endpoint):
     yield (reference, resource)
 
     if k8s.get_resource_exists(reference):
-        _, deleted = k8s.delete_custom_resource(job_definition_reference, 3, 10)
+        _, deleted = k8s.delete_custom_resource(reference, 3, 10)
         assert deleted
 
 
-def describe_sagemaker_model_explainability_job_definition(
+def get_sagemaker_model_explainability_job_definition(
     sagemaker_client, job_definition_name
 ):
     try:
@@ -83,7 +83,7 @@ class TestModelExplainabilityJobDefinition:
         job_definition_name = resource["spec"].get("jobDefinitionName")
         assert (
             k8s.get_resource_arn(resource)
-            == describe_sagemaker_model_explainability_job_definition(
+            == get_sagemaker_model_explainability_job_definition(
                 sagemaker_client, job_definition_name
             )["JobDefinitionArn"]
         )
@@ -92,7 +92,7 @@ class TestModelExplainabilityJobDefinition:
         _, deleted = k8s.delete_custom_resource(reference, 3, 10)
         assert deleted
         assert (
-            describe_sagemaker_model_explainability_job_definition(
+            get_sagemaker_model_explainability_job_definition(
                 sagemaker_client, job_definition_name
             )
             is None
