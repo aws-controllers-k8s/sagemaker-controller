1367c76427a2dd47adbd79743348205fc1803e34
BriannaRoskind <65782702+BriannaRoskind@users.noreply.github.com>
2021-08-06 12:57:50 -0700

    [Feature Group] Unit Testing (#68)
    Note:
    This PR is meant to be merged on top of [PR #66](https://github.com/aws-controllers-k8s/sagemaker-controller/pull/66).
    
    Summary:
    Added unit test to feature group for Create, ReadOne, and Delete (Feature Group does not support Update). Includes testing of state transitions. Code coverage for Feature Group = 53.9%.
    
    Total Code Coverage: 3.1%
    
    Files Added:
    
    - pkg/resource/feature_group/testdata/feature_group/v1alpha1 - Contains yaml files to be used in the desired and expected fields for testing scenarios in test_suite.yaml. (8 files)
       - fg_before_create.yaml
       - fg_create_failed_state.yaml
       - fg_create_initiated.yaml
       - fg_created_state.yaml
       - fg_creating_state.yaml
       - fg_deleting_state.yaml
       - fg_invalid_before_create.yaml
       - fg_invalid_create_attempted.yaml
    - pkg/resource/feature_group/testdata/feature_group/create - Contains json files to be used in the output_fixture fields for testing scenarios in test_suite.yaml which invoke the Create operation. (1 file)
       - fg_before_create.json
    - pkg/resource/feature_group/testdata/feature_group/read_one- Contains json files to be used in the output_fixture fields for testing scenarios in test_suite.yaml which invoke the ReadOne operation. (4 files)
       - fg_create_failed.json
       - fg_created.json
       - fg_creating.json
       - fg_deleting.json
    
    Files Edited:
    
    - pkg/common/custom_conditions.go - fixed bug revealed by unit test (needed to remove the space in "Create Failed" to do a compare to the terminal state)
    - pkg/resource/feature_group/manager_test_suite_test.go - Added YamlEqual that is used in test_suite_runner.go and compares the expected and actual resource as yaml files, presenting a diff output (and a copy of the actual output) which is easy to understand and use to modify the yaml files for testing. Also changed the Equal function to ignore the "LastTransitionTime" since as a timestamp it is updated while testing, and cannot be correctly preset in the expected output yaml file.
    - pkg/resource/feature_group/testdata/test_suite.yaml - Added state transition testing.
    - pkg/testutil/test_suite_runner.go - Added assert for the YamlEqual function to test the outputs of unit testing, and added YamlEqual to the TestRunnerDelegate interface. Also added option for unit testing scenario sdkapi mocking to have no provided output_fixture or error, and therefore mock both as being nil.

--- pkg/resource/feature_group/manager_test_suite_test.go
+++ pkg/resource/feature_group/manager_test_suite_test.go
@@ -16,6 +16,12 @@ package feature_group
 import (
 	"errors"
 	"fmt"
+	"os"
+	"io/ioutil"
+	"os/exec"
+
+	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
+	"github.com/ghodss/yaml"
 	mocksvcsdkapi "github.com/aws-controllers-k8s/sagemaker-controller/test/mocks/aws-sdk-go/sagemaker"
 	"github.com/aws-controllers-k8s/sagemaker-controller/pkg/testutil"
 	acktypes "github.com/aws-controllers-k8s/runtime/pkg/types"
@@ -27,6 +33,12 @@ import (
 	ctrlrtzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 	ackmetrics "github.com/aws-controllers-k8s/runtime/pkg/metrics"
 	"go.uber.org/zap/zapcore"
+)
+
+var (
+	DefaultTimestamp = "0001-01-01T00:00:00Z"
+	ReplaceTimestampRegExp = "s/\"[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z\"/\"" + DefaultTimestamp + "\"/"
+	TestDataDirectory = "testdata"
 )
 
 // provideResourceManagerWithMockSDKAPI accepts MockSageMakerAPI and returns pointer to resourceManager
@@ -48,8 +60,8 @@ func provideResourceManagerWithMockSDKAPI(mockSageMakerAPI *mocksvcsdkapi.SageMa
 	}
 }
 
-// TestDeclarativeTestSuite runs the test suite for feature group
-func TestDeclarativeTestSuite(t *testing.T) {
+// TestFeatureGroupTestSuite runs the test suite for feature group
+func TestFeatureGroupTestSuite(t *testing.T) {
      	defer func() {
      	   if r := recover(); r != nil {
      	      fmt.Println(testutil.RecoverPanicString, r)
@@ -88,7 +100,7 @@ func (d *testRunnerDelegate) EmptyServiceAPIOutput(apiName string) (interface{},
 	switch apiName {
 	case "CreateFeatureGroupWithContext":
 		var output svcsdk.CreateFeatureGroupOutput
-	    	return &output, nil
+		return &output, nil
 	case "DeleteFeatureGroupWithContext":
 		var output svcsdk.DeleteFeatureGroupOutput
 		return &output, nil
@@ -102,7 +114,8 @@ func (d *testRunnerDelegate) EmptyServiceAPIOutput(apiName string) (interface{},
 func (d *testRunnerDelegate) Equal(a acktypes.AWSResource, b acktypes.AWSResource) bool {
 	ac := a.(*resource)
 	bc := b.(*resource)
-	opts := []cmp.Option{cmpopts.EquateEmpty()}
+	// Ignore LastTransitionTime since it gets updated each run.
+	opts := []cmp.Option{cmpopts.EquateEmpty(), cmpopts.IgnoreFields(ackv1alpha1.Condition{}, "LastTransitionTime")}
 
 	if cmp.Equal(ac.ko.Status, bc.ko.Status, opts...) {
 		return true
@@ -111,4 +124,79 @@ func (d *testRunnerDelegate) Equal(a acktypes.AWSResource, b acktypes.AWSResourc
 		fmt.Println(cmp.Diff(ac.ko.Status, bc.ko.Status, opts...))
 		return false
 	}
+}
+
+// Checks to see if the given yaml file, with name stored as expectation,
+// matches the yaml marshal of the AWSResource stored as actual.
+func (d *testRunnerDelegate) YamlEqual(expectation string, actual acktypes.AWSResource) bool {
+     	// Get the file name of the expected yaml.
+	expectedYamlFileName := TestDataDirectory + "/" + expectation
+
+	// Build a tmp file for the actual yaml.
+	actualResource := actual.(*resource)
+	actualYamlByteArray, _ := yaml.Marshal(actualResource.ko)
+	actualYamlFileName := buildTmpFile("actualYaml", actualYamlByteArray)
+	defer os.Remove(actualYamlFileName)
+	if "" == actualYamlFileName {
+	  fmt.Printf("Could not create temporary actual file.\n")
+	  return false
+	}
+
+	// Replace Timestamps that would show up as different.
+	_, err := exec.Command("sed", "-r", "-i", ReplaceTimestampRegExp, actualYamlFileName).Output()
+	if isExecCommandError(err) {
+	    return false
+	}
+	
+	output,err := exec.Command("diff", "-c", expectedYamlFileName, actualYamlFileName).Output()
+	if isExecCommandError(err) {
+	   return false
+	}
+
+	if len(output) > 0 {
+	   actualOutput,err := exec.Command("cat", actualYamlFileName).Output()
+	   if isExecCommandError(err) {
+	      return false
+	   }
+	   fmt.Printf("\nExpected Yaml File Name: " + expectedYamlFileName + "\n")
+	   fmt.Printf("\nActual Output Yaml:\n" + string(actualOutput) + "\n")
+	   fmt.Printf("Diff From Expected:\n" + string(output) + "\n")
+	   return false
+	}
+	return true
+}
+
+func buildTmpFile(fileNameBase string, contents []byte) string {
+     newTmpFile, err := ioutil.TempFile(TestDataDirectory, fileNameBase)
+     if err != nil {
+        fmt.Println(err)
+	return ""
+     }
+     if _, err := newTmpFile.Write(contents); err != nil {
+        fmt.Println(err)
+	return ""
+     }
+     if err := newTmpFile.Close(); err != nil {
+        fmt.Println(err)
+	return ""
+     }		
+     return newTmpFile.Name()
+}
+
+// isExecCommandError returns true if an error
+// that is not an ExitError is found.
+func isExecCommandError(err error) bool {
+     if err == nil {
+     	return false
+     }
+     switch err.(type) {
+	case *exec.ExitError:
+	       // ExitError is expected.
+	       return false
+	default:
+	       // Couldn't run diff.
+	       fmt.Printf("Exec Command Error: ")
+	       fmt.Println(err)
+	       return true
+     }
 }
