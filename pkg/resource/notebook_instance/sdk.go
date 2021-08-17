829b6fb2afd732ef8b00c715efc17fff32de0bde
ananth102 <ananthbashyam1@gmail.com>
2021-08-17 19:29:04 +0000

    fixed formatting issues

--- pkg/resource/notebook_instance/sdk.go
+++ pkg/resource/notebook_instance/sdk.go
@@ -344,8 +344,7 @@ func (rm *resourceManager) sdkUpdate(
 	latestStatus := latest.ko.Status.NotebookInstanceStatus
 	// The Notebook Instance is stopped and the StoppedByControllerMetadata status is
 	// set to UpdatePending.
-	if latestStatus != nil &&
-		*latestStatus == svcsdk.NotebookInstanceStatusInService {
+	if latestStatus != nil && *latestStatus == svcsdk.NotebookInstanceStatusInService {
 		if err := rm.stopNotebookInstance(latest); err != nil {
 			return nil, err
 		} else {
