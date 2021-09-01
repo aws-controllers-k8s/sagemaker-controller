56f4892c2d629f55418ad6a2811709ccf8f048bf
Suraj Kota <surakota@amazon.com>
2021-09-01 00:11:44 +0000

    update conditions on delete and aws error code+message

--- pkg/resource/model/sdk.go
+++ pkg/resource/model/sdk.go
@@ -568,7 +568,7 @@ func (rm *resourceManager) updateConditions(
 			errorMessage = err.Error()
 		} else {
 			awsErr, _ := ackerr.AWSError(err)
-			errorMessage = awsErr.Message()
+			errorMessage = awsErr.Error()
 		}
 		terminalCondition.Status = corev1.ConditionTrue
 		terminalCondition.Message = &errorMessage
@@ -591,7 +591,7 @@ func (rm *resourceManager) updateConditions(
 			awsErr, _ := ackerr.AWSError(err)
 			errorMessage := err.Error()
 			if awsErr != nil {
-				errorMessage = awsErr.Message()
+				errorMessage = awsErr.Error()
 			}
 			recoverableCondition.Message = &errorMessage
 		} else if recoverableCondition != nil {
