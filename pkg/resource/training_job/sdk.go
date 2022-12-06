7e7398e1aa558417177bd1b0d54a0c41913cb80e
ananth102 <abashyam@amazon.com>
2022-12-06 03:08:25 +0000

    fix: setting the status of desired

--- pkg/resource/training_job/sdk.go
+++ pkg/resource/training_job/sdk.go
@@ -1189,6 +1189,11 @@ func (rm *resourceManager) sdkUpdate(
 	}
 
 	rm.setStatusDefaults(ko)
+	observed, err := rm.sdkFind(ctx, latest)
+	if err != nil {
+		return observed, err
+	}
+	desired.SetStatus(observed)
 	return desired, ackrequeue.NeededAfter(
 		errors.New("training job is updating"),
 		ackrequeue.DefaultRequeueAfterDuration,
--- templates/training_job/sdk_update_post_set_output.go.tpl
+++ templates/training_job/sdk_update_post_set_output.go.tpl
@@ -1,3 +1,8 @@
+observed, err := rm.sdkFind(ctx, latest)
+if err != nil {
+    return observed, err
+}
+desired.SetStatus(observed)
 return desired, ackrequeue.NeededAfter(
 	errors.New("training job is updating"),
 	ackrequeue.DefaultRequeueAfterDuration,
