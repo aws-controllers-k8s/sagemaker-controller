38998a0ce0c3df0dca416c35882295f26a9c705a
rd-pong <rdpeng@amazon.com>
2023-04-18 10:16:41 -0700

    Address comments Delete placeholder values Remove endpoint config test in shallow canary Change mnist.pkl.gz directory

--- test/e2e/replacement_values.py
+++ test/e2e/replacement_values.py
@@ -75,10 +75,6 @@ DEBUGGER_IMAGE_URIS = {
     "eu-south-1": "563282790590.dkr.ecr.eu-south-1.amazonaws.com",
     "ap-northeast-3": "479947661362.dkr.ecr.ap-northeast-3.amazonaws.com",
     "af-south-1": "314341159256.dkr.ecr.af-south-1.amazonaws.com",
-    "eu-south-2": "",
-    "ap-southeast-3": "",
-    "me-central-1": "",
-    "eu-central-2": "",
 }
 
 # https://docs.aws.amazon.com/sagemaker/latest/dg/sagemaker-algo-docker-registry-paths.html
@@ -170,9 +166,6 @@ MODEL_MONITOR_IMAGE_URIS = {
     "us-gov-west-1": "362178532790.dkr.ecr.us-gov-west-1.amazonaws.com",
     "ap-northeast-3": "990339680094.dkr.ecr.ap-northeast-3.amazonaws.com",
     "ap-southeast-3": "669540362728.dkr.ecr.ap-southeast-3.amazonaws.com",
-    "eu-south-2": "",
-    "me-central-1": "",
-    "eu-central-2": "",
 }
 
 # https://docs.aws.amazon.com/sagemaker/latest/dg/clarify-configure-processing-jobs.html#clarify-processing-job-configure-container
@@ -199,9 +192,6 @@ CLARIFY_IMAGE_URIS = {
     "eu-south-1": "638885417683.dkr.ecr.eu-south-1.amazonaws.com",
     "ap-northeast-3": "912233562940.dkr.ecr.ap-northeast-3.amazonaws.com",
     "ap-southeast-3": "705930551576.dkr.ecr.ap-southeast-3.amazonaws.com",
-    "eu-south-2": "",
-    "me-central-1": "",
-    "eu-central-2": "",
 }
 
 ENDPOINT_INSTANCE_TYPES = {
