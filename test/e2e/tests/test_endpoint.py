4c366b17c67f0785e2cca73889b8a8886bd27981
rd-pong <rdpeng@amazon.com>
2023-04-18 13:07:26 -0700

    [Cherry-pick] Create shallow canary marker and fix "No S3 objects found under S3" (#242)
        - Create shallow canary marker for eu-south-2, ap-southeast-3,
        me-central-1, eu-central-2, since some registries are not available in
        those regions.
        - Add training and hosting to shallow canary
        - Add `--region ` for `aws s3 sync` to prevent error when downloading s3
        bucket data to temp directory.
        - Remove dependency on `s3://sagemaker-sample-data-$AWS_REGION `

--- test/e2e/tests/test_endpoint.py
+++ test/e2e/tests/test_endpoint.py
@@ -218,6 +218,7 @@ def faulty_config(name_suffix, single_container_model):
 
 
 @service_marker
+@pytest.mark.shallow_canary
 @pytest.mark.canary
 class TestEndpoint:
     def create_endpoint_test(self, xgboost_endpoint):
