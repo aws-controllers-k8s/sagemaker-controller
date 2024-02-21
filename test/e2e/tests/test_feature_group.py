168728bce225844ecb936afeb48ae5aef8c07ca7
Suryansh Singh <suryans@amazon.com>
2024-02-20 16:56:49 -0800

    Increase wait on condition timeout for Feature Store and Studio.

--- test/e2e/tests/test_feature_group.py
+++ test/e2e/tests/test_feature_group.py
@@ -38,7 +38,7 @@ SPEC_FILE = "feature_group"
 FEATURE_GROUP_STATUS_CREATING = "Creating"
 FEATURE_GROUP_STATUS_CREATED = "Created"
 # longer wait is used because we sometimes see server taking time to create/delete
-WAIT_PERIOD_COUNT = 9
+WAIT_PERIOD_COUNT = 15
 WAIT_PERIOD_LENGTH = 30
 STATUS = "status"
 RESOURCE_STATUS = "featureGroupStatus"
