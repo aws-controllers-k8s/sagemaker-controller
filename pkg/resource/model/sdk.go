63768ef3a7bbb5a0ecd976b76db344c59e1108a0
rkurduka <rkurduka@amazon.com>
2024-03-01 10:20:39 +0000

    fixed update modeldatasource issue in sdkfind

--- pkg/resource/model/sdk.go
+++ pkg/resource/model/sdk.go
@@ -90,8 +90,11 @@ func (rm *resourceManager) sdkFind(
 	// the original Kubernetes object we passed to the function
 	ko := r.ko.DeepCopy()
 
+	// This is require if only spec.primarycontainer.modeldataurl is in use and not the ko.spec.primarycontainer.ModelDataSource
+	// in this case, during find "ko.spec.primarycontainer.ModelDataSource" gets updated as well , which creates a new k8s generation
+
 	if ko.Spec.PrimaryContainer.ModelDataSource == nil && resp.PrimaryContainer.ModelDataSource != nil {
-		resp.PrimaryContainer.ModelDataSource = nil	
+		resp.PrimaryContainer.ModelDataSource = nil
 	}
 
 	if resp.Containers != nil {
