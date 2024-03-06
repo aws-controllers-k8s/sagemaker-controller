99037c217949d2cf94caa2c273d4a1d10c73a9e9
rkurduka <rkurduka@amazon.com>
2024-03-06 09:01:11 +0000

    added check to validate field for update

--- pkg/resource/model/sdk.go
+++ pkg/resource/model/sdk.go
@@ -633,6 +633,16 @@ func (rm *resourceManager) sdkUpdate(
 	defer func() {
 		exit(err)
 	}()
+	// checking if any delta is found other than tags
+	if len(delta.Differences) > 0 {
+		if delta.DifferentExcept("Spec.Tags") {
+			for _, parts := range delta.Differences {
+				if !parts.Path.Contains("Tags") {
+					return nil, fmt.Errorf("cannot update the following fields: %s , Allowed field to change: Spec.Tags", parts.Path)
+				}
+			}
+		}
+	}
 	// this to handle delete/remove tags
 	_, err = rm.deleteTags(ctx, desired, latest)
 	if err != nil {
