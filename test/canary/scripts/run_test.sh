19d2a7bbd5ca7cacb877c63d0fb5c972d382c116
Suryansh Singh <suryans@amazon.com>
2024-03-13 16:46:08 -0700

    Reverse the condition

--- test/canary/scripts/run_test.sh
+++ test/canary/scripts/run_test.sh
@@ -103,10 +103,10 @@ pushd $E2E_DIR
   else
     pytest_marks="canary"
   fi
-  if ! [[ $SERVICE_REGION =~ ^(eu-north-1|ap-south-1|ap-southeast-3|us-east-2|me-central-1|eu-west-1|eu-central-1|sa-east-1|us-east-1|ap-northeast-2|eu-west-2|ap-northeast-1|us-west-2|ap-southeast-1|ap-southeast-2|ca-central-1)$  ]]; then
-    # Above is the list of supported regions and if the current region is not included in this
-    # will remove the inference_component mark.
-      pytest_marks+=" not inference_component"
+  if [[ $SERVICE_REGION =~ ^(eu-north-1|ap-south-1|ap-southeast-3|us-east-2|me-central-1|eu-west-1|eu-central-1|sa-east-1|us-east-1|ap-northeast-2|eu-west-2|ap-northeast-1|us-west-2|ap-southeast-1|ap-southeast-2|ca-central-1)$  ]]; then
+    # Above is the list of supported regions and if the current region is included in this
+    # we will add the inference_component mark.
+      pytest_marks+=" or inference_component"
   fi
   pytest_args+=(-m "$pytest_marks")
   pytest "${pytest_args[@]}"
