106575ab874f60d738878e4c745f1ce7dcd1a5d5
Ananth Bashyam <abashyam@amazon.com>
2024-10-22 23:55:33 +0000

    Adding timeout for integration test

--- test/e2e/tests/test_inference_component.py
+++ test/e2e/tests/test_inference_component.py
@@ -207,6 +207,7 @@ def faulty_model(name_suffix, xgboost_model):
 
 @service_marker
 @pytest.mark.inference_component
+@pytest.mark.timeout(3600) # 1 hour timeout
 class TestInferenceComponent:
     def create_inference_component_test(self, inference_component):
         (reference, resource, _) = inference_component
