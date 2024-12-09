f3dc4e7f1c989a9bcdd7e7030bbe241a028bf022
ci-robot <ci-robot@k8s.io>
2024-12-09 17:40:22 +0000

    Update to ACK runtime `v0.40.0`, code-generator `v0.40.0`

--- pkg/resource/user_profile/resource.go
+++ pkg/resource/user_profile/resource.go
@@ -98,6 +98,22 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 	return nil
 }
 
+// PopulateResourceFromAnnotation populates the fields passed from adoption annotation
+func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
+	tmp, ok := fields["userProfileName"]
+	if !ok {
+		return ackerrors.MissingNameIdentifier
+	}
+	r.ko.Spec.UserProfileName = &tmp
+
+	f0, f0ok := fields["domainID"]
+	if f0ok {
+		r.ko.Spec.DomainID = &f0
+	}
+
+	return nil
+}
+
 // DeepCopy will return a copy of the resource
 func (r *resource) DeepCopy() acktypes.AWSResource {
 	koCopy := r.ko.DeepCopy()
