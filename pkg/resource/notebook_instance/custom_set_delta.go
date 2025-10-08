b2299c0c290b9d1083580e4e800a4fb401d97b2f
sirutBuasai <sirutbuasai27@outlook.com>
2025-10-08 22:17:16 +0000

    use late init for notebook resource

--- pkg/resource/notebook_instance/custom_set_delta.go
+++ pkg/resource/notebook_instance/custom_set_delta.go
@@ -10,12 +10,10 @@
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
-
 package notebook_instance
 
 import (
 	ackcompare "github.com/aws-controllers-k8s/runtime/pkg/compare"
-	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 // customSetDefaults sets the default fields for DirectInternetAccess and RootAccess.
@@ -25,22 +23,18 @@ func customSetDefaults(
 ) {
 	// Direct Internet Access describes whether Amazon SageMaker provides internet access to the notebook instance.
 	// The default value is Enabled.
-	DefaultDirectInternetAccess := aws.String("Enabled")
 	if ackcompare.IsNil(a.ko.Spec.DirectInternetAccess) && ackcompare.IsNotNil(b.ko.Spec.DirectInternetAccess) {
-		a.ko.Spec.DirectInternetAccess = DefaultDirectInternetAccess
+		a.ko.Spec.DirectInternetAccess = b.ko.Spec.DirectInternetAccess
 	}
-
 	// Root Access describes whether root access is enabled or disabled for users of the notebook instance.
 	// The default value is Enabled
-	DefaultRootAccess := aws.String("Enabled")
 	if ackcompare.IsNil(a.ko.Spec.RootAccess) && ackcompare.IsNotNil(b.ko.Spec.RootAccess) {
-		a.ko.Spec.RootAccess = DefaultRootAccess
+		a.ko.Spec.RootAccess = b.ko.Spec.RootAccess
 	}
 
 	// IPAddressType specify either dualstack ipv4/ipv6 or just ipv4
 	// The default value is ipv4
-	DefaultIPAddressType := aws.String("ipv4")
 	if ackcompare.IsNil(a.ko.Spec.IPAddressType) && ackcompare.IsNotNil(b.ko.Spec.IPAddressType) {
-		a.ko.Spec.RootAccess = DefaultIPAddressType
+		a.ko.Spec.RootAccess = b.ko.Spec.IPAddressType
 	}
 }
