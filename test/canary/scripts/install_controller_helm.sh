6dfdd8c71746bfec72a3931402b8b83e57dfe5c4
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-24 05:11:45 -0500

    fix role arn

--- test/canary/scripts/install_controller_helm.sh
+++ test/canary/scripts/install_controller_helm.sh
@@ -9,7 +9,7 @@ function install_helm_chart() {
     local namespace="$4"
 
     # yq eval '.serviceAccount.annotations = ""' -i helm/values.yaml
-    yq eval ".serviceAccount.annotations.\"eks.amazonaws.com\" = \"$oidc_role_arn\"" -i helm/values.yaml
+    yq eval ".serviceAccount.annotations.\"eks.amazonaws.com/role-arn\" = \"$oidc_role_arn\"" -i helm/values.yaml
     yq eval ".aws.region = \"$region\"" -i helm/values.yaml
     yq eval '.log.level = "debug"' -i helm/values.yaml
     yq eval '.log.enable_development_logging = true' -i helm/values.yaml
