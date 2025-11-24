d83367273998892464fea5b117165d60d5cb1512
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-24 07:20:33 -0500

    add yq annotation

--- test/canary/scripts/install_controller_helm.sh
+++ test/canary/scripts/install_controller_helm.sh
@@ -8,6 +8,7 @@ function install_helm_chart() {
     local region="$3"
     local namespace="$4"
 
+    yq eval ".serviceAccount.annotations = {}" -i helm/values.yaml
     yq eval ".serviceAccount.annotations.\"eks.amazonaws.com/role-arn\" = \"$oidc_role_arn\"" -i helm/values.yaml
     yq eval ".aws.region = \"$region\"" -i helm/values.yaml
     yq eval '.log.level = "debug"' -i helm/values.yaml
